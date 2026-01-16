bf2fc636f4fe933ffb19292d59f125b305d2bb0c
litonroy900796 <litonroy900796@gmail.com>
2026-01-17 01:47:44 +0600

    home page post add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { initialState, postReducer } from "../reducers/PostReducer";
+import useAxios from "../hooks/useAxios";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
 
 function HomePage() {
-  return <div></div>;
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+  console.log("state", state);
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+    const fetchPost = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`,
+        );
+        if (response.status === 200) {
+          dispatch({
+            type: actions.post.DATA_FETCHED,
+            data: response.data,
+          });
+        }
+      } catch (error) {
+        console.error(error);
+        dispatch({
+          type: actions.post.DATA_FETCH_ERROR,
+          error: error.message,
+        });
+      }
+    };
+
+    fetchPost();
+  }, [dispatch, api]);
+  return <div>{<PostList posts={state.posts} />}</div>;
 }
 
 export default HomePage;
